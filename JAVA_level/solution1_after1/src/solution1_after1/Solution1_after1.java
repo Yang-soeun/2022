ab3ba6f937e7824a168a5249b76189716c14eb15
Yang-soeun <soeun8636@naver.com>
2022-02-13 02:56:39 +0900

    solution_1

---
+++ JAVA_level/solution1_after1/src/solution1_after1/Solution1_after1.java
@@ -0,0 +1,25 @@
+package solution1_after1;
+
+import java.util.Arrays;
+
+public class Solution1_after1 {
+	
+	public static int[] solution1(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		
+		for(int i = 0; i< commands.length; i++) {
+			int[] newArray = Arrays.copyOfRange(array, commands[i][0] -1, commands[i][1]);
+			Arrays.sort(newArray);
+			answer[i] = newArray[commands[i][2] -1];
+		}
+		return answer;
+	} 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] a = {1, 5, 2, 6, 3, 7, 4};
+        int[][] c = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+		
+        System.out.println(Arrays.toString(solution1(a, c)));
+	}
+
+}
