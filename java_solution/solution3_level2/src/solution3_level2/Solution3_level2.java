12f1d605da1c56b26c21d6e4a78ff6ec6450b5eb
Yang-soeun <soeun8636@naver.com>
2022-02-20 07:36:02 +0900

    수정_solution3_level2

--- java_solution/solution3_level2/src/solution3_level2/Solution3_level2.java
+++ java_solution/solution3_level2/src/solution3_level2/Solution3_level2.java
@@ -5,22 +5,25 @@ import java.util.Iterator;
 
 public class Solution3_level2 {
 	HashSet<Integer> set = new HashSet<>();
-
 	public int solution(String numbers) {
+
 		int answer =0;
 		
 		permutation("", numbers);
 		Iterator<Integer> it =set.iterator();
 		
+		//System.out.println(set);
 		while(it.hasNext()) {
 			int number = it.next();
+			int n = number;
 			if(isPrime(number)) answer++;
 		}
 		return answer;
 	}
 	//소수를 판별하는 함수
+	//에라토스테네스의 체
     public boolean isPrime(int num){
-        if (num == 0 || num == 1) { return false;} 
+        if (num == 0 || num == 1) { return false;}//소수가 아니므로
         
         for (int i=2;i<=(int)Math.sqrt(num);i++){
             if (num % i ==0){
@@ -37,6 +40,7 @@ public class Solution3_level2 {
 		
 		for(int i = 0; i<numbers.length(); i++) {
 			permutation(a + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i+1));
+			//System.out.println(numbers.substring(0, i) + numbers.substring(i+1));
 		}
 	}
 	
